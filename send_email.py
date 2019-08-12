09c0c430e6cd7459a1cfb9b70842cf23616ec087
ChrisStreadbeck <han0987@gmail.com>
2019-08-12 11:44:37 -0600

    pushing survey app

---
+++ send_email.py
@@ -0,0 +1,22 @@
+from email.mime.text import MIMEText
+import smtplib 
+
+def send_email(email, height, average_height, count):
+    from_email="c.streadbeck01@gmail.com"
+    from_password="thronk86"
+    to_email=email
+
+    subject="Height Data"
+    message="Hey there, your height is <strong>%s</strong> inches. <br> Average height of everyone so far is <strong>%s</strong> from <strong>%s</strong> people. <br> Thanks!" % (height, average_height, count)
+    
+
+    msg=MIMEText(message, 'html')
+    msg['Subject']=subject
+    msg['To']=to_email
+    msg['From']=from_email
+
+    gmail=smtplib.SMTP('smtp.gmail.com',587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(from_email, from_password)
+    gmail.send_message(msg)
